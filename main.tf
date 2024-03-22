2fb5b658ebe38fa5f7d2269e721dc26e7c5a81c4
Xavier Venteo <xavierventeo@gmail.com>
2024-03-22 13:30:53 +0100

    Terraform code using aws provider and aws_secretsmanager_secret

---
+++ main.tf
@@ -0,0 +1,20 @@
+# AWS provider configuration
+provider "aws" {
+  region = "eu-west-1" # Europe (Ireland)
+}
+
+# Retreive sensitive Allowed IPs record from AWS Secrets Manager
+data "aws_secretsmanager_secret" "allowed_ip_address" {
+  name = "AllowedIpAddress"
+}
+data "aws_secretsmanager_secret_version" "allowed_ip_address_version" {
+  secret_id = data.aws_secretsmanager_secret.allowed_ip_address.id
+}
+
+# Get the vault value and store it in a local variable could be use in the terraform file
+locals {
+  allowed_ip = jsondecode(data.aws_secretsmanager_secret_version.allowed_ip_address_version.secret_string)["allowed_ip_address"]
+}
+
+# Refer allowed_ip local variable as: [local.allowed_ip]]
+# Protected by design to print vaults using output
